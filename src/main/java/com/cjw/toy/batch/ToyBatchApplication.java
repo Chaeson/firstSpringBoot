ac4727b95088fc8751e670a0cf3348b3e4821154
Chae JongWook <only9984@gmail.com>
2020-12-08 23:04:45 +0900

    [ADD] - Job에 대한 클래스를 생성해 보았으나, 여전히 실패가 되었다. - 책을 보면서 하나씩 따라해보고 실행해보면서 배워나가려고 한다.   참고문헌: 처음 배우는 스프링부터 2.0(한빛 미디어)

--- src/main/java/com/cjw/toy/batch/ToyBatchApplication.java
+++ src/main/java/com/cjw/toy/batch/ToyBatchApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class ToyBatchApplication {
     public static void main(String[] args){
         SpringApplication.run(ToyBatchApplication.class, args);
--- src/main/java/com/cjw/toy/batch/launch/ToyBatchJobLauncher.java
+++ src/main/java/com/cjw/toy/batch/launch/ToyBatchJobLauncher.java
@@ -1,10 +1,9 @@
 package com.cjw.toy.batch.launch;
 
+import com.cjw.toy.batch.job.ToyJob;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -13,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-@Configuration
+@EnableBatchProcessing
 public class ToyBatchJobLauncher {
 
     JobLauncher jobLauncher;
@@ -26,6 +25,8 @@ public class ToyBatchJobLauncher {
                         .addLong("time",System.currentTimeMillis())
                         .toJobParameters();
         log.info("jobParameters::{}",jobParameters.toString());
-        jobLauncher.run(job, jobParameters);
+        jobLauncher.run(job,jobParameters);
     }
+
+
 }
