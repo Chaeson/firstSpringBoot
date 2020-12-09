3949f2694abb483bf710456e1ee391ef80c38776
Chae JongWook <only9984@gmail.com>
2020-12-10 00:15:22 +0900

    [ADD] - 오늘의 뻘  > Maven에서 Gradle로 바꾸기 - 오늘의 이슈  > JUnit으로 테스트 해보기  > 없는 Dependecy에서 실행하려다 보니 뭔가 에러가 많다...  > 다 잡아보자!

--- src/main/java/com/cjw/toy/ToyApplication.java
+++ src/main/java/com/cjw/toy/ToyApplication.java
@@ -2,12 +2,20 @@ package com.cjw.toy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController         // 테스트 작동 확인.
 @SpringBootApplication
 public class ToyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ToyApplication.class, args);
+    }
+
+    @GetMapping
+    public String HelloWorld(){
+        return "Hello World";
     }
 
 }
--- src/test/java/com/cjw/toy/ToyApplicationTests.java
+++ src/test/java/com/cjw/toy/ToyApplicationTests.java
@@ -1,13 +1,47 @@
 package com.cjw.toy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ToyApplicationTests {
 
+    @Value("${property.test.name}")
+    private String propertyTestName;
+
+    @Value("${propertyTest}")
+    private String propertyTest;
+
+    @Value("${noKey:default value}")
+    private String defaultValue;
+
+    @Value("${propertyTestList}")
+    private String[] propertyTestArray;
+
+    @Value("#{'${propertyTestList}'.split(',')}")
+    private List<String> propertyTestList;
+
     @Test
-    void contextLoads() {
+    public void test() {
+        assertThat(propertyTestName, is("Value versus ConfigurationProperties"));
+        assertThat(propertyTest, is("Test"));
+        assertThat(defaultValue, is("default value"));
+        assertThat(propertyTestArray[0], is("Test1"));
+        assertThat(propertyTestArray[1], is("Test2"));
+        assertThat(propertyTestArray[2], is("Test3"));
+
+        assertThat(propertyTestList.get(0), is("Test1"));
+        assertThat(propertyTestList.get(1), is("Test2"));
+        assertThat(propertyTestList.get(2), is("Test3"));
     }
 
 }
